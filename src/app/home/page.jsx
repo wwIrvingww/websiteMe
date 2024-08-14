852446aade18c16b37ab49482bafbc05956b3d70
wwIrvingww <mor22781@uvg.edu.gt>
2024-08-14 00:53:24 -0600

    landing paged finished

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -9,7 +9,19 @@ const Home = () => {
       <div className={styles.title_container}>
         <h1>FABRICIO</h1>
       </div>
-
+      <div className={styles.designer_container}>
+        <h2>Web Developer</h2>
+      </div>
+      <div className={styles.ux_container}>
+        <h2>UX/UI Designer</h2>
+      </div>
+      <div className={styles.image_container}>
+        <img src={'https://i.pinimg.com/564x/c2/88/5b/c2885bb7d30c39a67dadc71a52d167fe.jpg'} alt="Portrait 1" />
+        <img src={'https://i.pinimg.com/564x/c2/88/5b/c2885bb7d30c39a67dadc71a52d167fe.jpg'} alt="Portrait 2" />
+      </div>
+      <div className={styles.shape_container}> 
+        <img src='./Shape1.png' alt='Shape 1'/>  {/* Asegúrate de usar <img> en lugar de <image> */}
+      </div>
     </div>
   );
 }
