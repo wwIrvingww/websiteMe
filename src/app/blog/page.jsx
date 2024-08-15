507bcca98382b521c54eb4577b2659a0fa611920
wwIrvingww <mor22781@uvg.edu.gt>
2024-08-15 09:49:18 -0600

    blog fixed

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
       <iframe
         src="https://myblog-irving-acostas-projects-400bd115.vercel.app/"
         className={styles.iframeBlog}
-        style={{ width: '100vw', height: '100vh', margin: 0, overflow: 'hidden' }} // Inyectando el ancho y alto directamente
+        style={{ width: '100vw', height: '100vh', margin: 0, overflow: 'hidden', border: 'none' }} // Agregando 'border: none'
       ></iframe>
     </div>
   );
