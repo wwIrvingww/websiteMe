b3320d595eb67b88a4621936d8e55bd5a8de229d
wwIrvingww <mor22781@uvg.edu.gt>
2024-09-17 09:18:24 -0600

    contact form integrated

--- src/app/zComponents/mobile/contact_form.jsx
+++ src/app/zComponents/mobile/contact_form.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import './contact_form.css';
 
 const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('default_service', 'template_706hhg9', form.current, 'gKwVdljT99tzjHHaG')
+      .then((result) => {
+          console.log(result.text);
+          alert("Mensaje enviado con éxito");
+      }, (error) => {
+          console.log(error.text);
+          alert("Hubo un error al enviar el mensaje");
+      });
+  };
+
   return (
     <div className="formContainer">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="formGroup">
           <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" name="name" className="formInput" />
+          <input type="text" id="name" name="user_name" className="formInput" required />
         </div>
         <div className="formGroup">
           <label htmlFor="phone">Your Phone</label>
-          <input type="text" id="phone" name="phone" className="formInput" />
+          <input type="text" id="phone" name="user_phone" className="formInput" />
         </div>
         <div className="formGroup">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" className="formInput" />
+          <input type="email" id="email" name="user_email" className="formInput" required />
         </div>
         <div className="formGroup">
           <label htmlFor="subject">Asunto</label>
           <input type="text" id="subject" name="subject" className="formInput" />
         </div>
         <div className="formGroup lastGroup">
-            <label htmlFor="message">Mensaje</label>
-            <textarea id="message" name="message" className="formTextarea"></textarea>
+          <label htmlFor="message">Mensaje</label>
+          <textarea id="message" name="message" className="formTextarea" required></textarea>
         </div>
         <button type="submit" className="formButton">SEND</button>
       </form>
