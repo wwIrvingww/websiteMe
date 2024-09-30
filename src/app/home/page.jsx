ef1dc814f9d6c50e06f8ef9a3ec21440b4776bf7
wwIrvingww <mor22781@uvg.edu.gt>
2024-09-30 11:47:19 -0600

    style: responsive design corrected

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -9,28 +9,7 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      {/* <div className={styles.vertical_line}></div>
-      <div className={styles.horizontal_line}></div>
-      <div className={styles.title_container}>
-        <h1 className={styles.title_link} onClick={toggleMenu}>FABRICIO</h1>
-      </div>
-      <div className={styles.designer_container}>
-        <h2>Web Developer</h2>
-      </div>
-      <div className={styles.ux_container}>
-        <h2>UX/UI Designer</h2>
-      </div>
-      <div className={styles.image_container}>
-        <img src={'./portrait.png'} alt="Portrait 1" />
-        <img src={'./portrait.png'} alt="Portrait 2" />
-      </div>
-      <div className={styles.shape_container}> 
-        <img src='./Shape1.png' alt='Shape 1'/> 
-      </div> */}
-
-    <Landing/>
-
-      
+    <Landing/>   
     </div>
   );
 }
